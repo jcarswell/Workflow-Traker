a3fb4458aa0c1c1b197248792b2da3f983d2b4d8
Josh Carswell <josh.carswell@thecarswells.ca>
2017-02-13 20:59:16 -0600

    Added favicon to static files to resolve issue with bad request created index page copied base template and static files from the root of the project modified the views so posting is happening correctly and not to the non submit page

--- exchange_transition/views.py
+++ exchange_transition/views.py
@@ -6,26 +6,31 @@ from .models import User, UserStep, Step
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        if request.session.test_cookie_worked():
-            users = User.objects.order_by('name')
-            request.session.delete_test_cookie()
-            if request.COOKIES.has_key('techname'):
-                techname = request.COOKIES['techname']
-            else:
-                techname = ""
-            return HttpResponse(render(request, 'exchange_transition/index.html', { 'users' : users, 'techname' : techname }))
-        else:
-            return HttpResponse("Cookies are required to use this site, please enable cookies and try again")
+    users = User.objects.order_by('name')
+    if request.COOKIES.has_key('techname'):
+        techname = request.COOKIES['techname']
+    else:
+        techname = ""
     request.session.set_test_cookie()
-    return HttpResponse(render(request, 'exchange_transition/index.html'))
+    context = { 
+        'users' : users,
+        'techname' : techname,
+    }
+
+    return HttpResponse(render(request, 'exchange_transition/index.html', context))
 
 def user(request, username):
+    if request.COOKIES.has_key('techname'):
+        techname = request.COOKIES['techname']
+    else:
+        return HttpResponse("Technician name not available, please return to the main page a try again")
+
     try:
         user = User.objects.get(name=username)
         steps = UserStep.objects.get(name=username)
     except Users.DoesNotExist:
         raise Http404("User %s does not exist" % username)
+    request.session.set_test_cookie()
 
     steps
     context = {
@@ -37,14 +42,29 @@ def user(request, username):
 def submit(request, username):
     if request.method != 'POST':
         return redirect(index)
-    if 
-    try:
-        user = User.objects.get(name=username)
+    else:
+        if request.COOKIES.has_key('techname'):
+            techname = request.COOKIES['techname']
+        else:
+            return HttpResponse("Technician name not available, please return to the main page a try again")
+
         try:
-            step = user.UserStep_set.get(step=request.POST['stepOrder'])
-        except user.UserStep_set.DoesNotExist:
-            pass
+            redirectURL = request.POST['redirect']
+        except:
+            redirectURL = ''
+
+        if request.session.test_cookie_worked():
+            request.session.delete_test_cookie()
+            return redirect(redirectURL)
+        else:
+            return HttpResponse("Cookies are required to use this site, please enable cookies and try again")
+        try:
+            user = User.objects.get(name=username)
+            try:
+                step = user.UserStep_set.get(step=request.POST['stepOrder'])
+            except user.UserStep_set.DoesNotExist:
+                raise Http404("Invalid step ID")
             # check if step exist or raise 404
-    except Users.DoesNotExist:
-        raise Http404("User %s does not exist" % username)
+        except Users.DoesNotExist:
+            raise Http404("User %s does not exist" % username)
 
