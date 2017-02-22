f639cee360de7f4bb3aa32b4bd45a53ef97a6fcf
Josh Carswell <josh.carswell@thecarswells.ca>
2017-02-21 23:06:10 -0600

    corrected the submit url to be representative of what is exptexted to happen

--- exchange_transition/urls.py
+++ exchange_transition/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='et_index'),
-    url(r'u/^(?P<user_alias>[a-zA-Z0-9\-_.]+)/$', views.user, name='et_user'),
-    url(r'u/^(?P<user_alias>[a-zA-Z0-9\-_.]+)/submit/$', views.submit, name='et_submit'),
+    url(r'u/^(?P<userAlias>[a-zA-Z0-9\-_.]+)/$', views.user, name='et_user'),
+    url(r'submit/$', views.submit, name='et_submit'),
     url(r'^Manage/$', views.manage_root, name='et_manage'),
     url(r'^Manage/users/$', views.manage_view_users, name='et_manage_view_users'),
     url(r'^Manage/steps/$', views.manage_view_steps, name='et_manage_view_steps'),
